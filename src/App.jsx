7b40b6cd69f654a46ed37a254a014c5ba81fff53
Segundavid-Dev <segdavid03@gmail.com>
2025-03-06 02:17:44 +0100

    creating more components

--- src/App.jsx
+++ src/App.jsx
@@ -48,11 +48,31 @@ const pizzaData = [
 function App() {
   return (
     <div>
-      <p>Hello react world!</p>;
+      <Header />
+      <Menu />
+      <Footer />
+    </div>
+  );
+}
+
+function Header() {
+  return <h1>Fast React Pizza Co.</h1>;
+}
+
+function Menu() {
+  return (
+    <div>
+      <h2>Our menu</h2>
       <Pizza />
       <Pizza />
       <Pizza />
     </div>
+  );
+}
+
+function Footer() {
+  return (
+    <footer>{new Date().toLocaleTimeString()} We're currently open</footer>
   );
 }
 
