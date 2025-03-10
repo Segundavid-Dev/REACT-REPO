fddbca76c7c70dd39ebead7bff433fd108354173
Segundavid-Dev <segdavid03@gmail.com>
2025-03-10 11:07:41 +0100

    rendering list as individual components

--- src/App.jsx
+++ src/App.jsx
@@ -5,42 +5,42 @@ const pizzaData = [
     name: "Focaccia",
     ingredients: "Bread with italian olive oil and rosemary",
     price: 6,
-    photoName: "pizzas/focaccia.jpg",
+    photoName: "src/assets/images/focaccia.jpg",
     soldOut: false,
   },
   {
     name: "Pizza Margherita",
     ingredients: "Tomato and mozarella",
     price: 10,
-    photoName: "pizzas/margherita.jpg",
+    photoName: "src/assets/images/margherita.jpg",
     soldOut: false,
   },
   {
     name: "Pizza Spinaci",
     ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
     price: 12,
-    photoName: "pizzas/spinaci.jpg",
+    photoName: "src/assets/images/spinaci.jpg",
     soldOut: false,
   },
   {
     name: "Pizza Funghi",
     ingredients: "Tomato, mozarella, mushrooms, and onion",
     price: 12,
-    photoName: "pizzas/funghi.jpg",
+    photoName: "src/assets/images/funghi.jpg",
     soldOut: false,
   },
   {
     name: "Pizza Salamino",
     ingredients: "Tomato, mozarella, and pepperoni",
     price: 15,
-    photoName: "pizzas/salamino.jpg",
+    photoName: "src/assets/images/salamino.jpg",
     soldOut: true,
   },
   {
     name: "Pizza Prosciutto",
     ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
     price: 18,
-    photoName: "pizzas/prosciutto.jpg",
+    photoName: "src/assets/images/prosciutto.jpg",
     soldOut: false,
   },
 ];
@@ -67,31 +67,24 @@ function Menu() {
   return (
     <main className="menu">
       <h2>Our menu</h2>
-      <Pizza
-        name="Pizza Spinaci"
-        ingridents="Tomato, mozarella, spinach, and ricotta cheese"
-        photoName="src/assets/images/spinaci.jpg"
-        price={10}
-      />
 
-      <Pizza
-        name="Focaccia"
-        ingridents="Bread with italian olive oil and rosemary"
-        photoName="src\assets\images\focaccia.jpg"
-        price={12}
-      />
+      <ul className="pizzas">
+        {pizzaData.map((pizza) => (
+          <Pizza pizzaObj={pizza} key={pizza.name} />
+        ))}
+      </ul>
     </main>
   );
 }
 
 function Pizza(props) {
   return (
-    <div className="pizza">
-      <h3>{props.name}</h3>
-      <img src={props.photoName} alt={props.name} />
-      <p>{props.ingridents}</p>
-      <span>{props.price + 3}</span>
-    </div>
+    <li className="pizza">
+      <h3>{props.pizzaObj.name}</h3>
+      <img src={props.pizzaObj.photoName} alt={props.pizzaObj.name} />
+      <p>{props.pizzaObj.ingredients}</p>
+      <span>{props.pizzaObj.price + 3}</span>
+    </li>
   );
 }
 
