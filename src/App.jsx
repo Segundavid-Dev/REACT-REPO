9255b73bef33b891dbf1630222eb0be71d8f16fc
Segundavid-Dev <segdavid03@gmail.com>
2025-03-12 03:20:49 +0100

    completed another project

--- src/App.jsx
+++ src/App.jsx
@@ -1,64 +1,44 @@
 import { useState } from "react";
 import "./App.css";
 
-const messages = [
-  "Learn React ⚛️",
-  "Apply for jobs 💼",
-  "Invest your new income 🤑",
-];
-
 function App() {
-  const [step, setStep] = useState(1);
-  const [isOpen, setIsOpen] = useState(true);
-
-  function handlePrevious() {
-    if (step > 1) {
-      setStep((s) => s - 1);
-    }
-  }
-
-  function handleNext() {
-    if (step < 3) {
-      setStep((s) => s + 1);
-    }
-  }
-
   return (
-    <>
-      <button className="close" onClick={() => setIsOpen(!isOpen)}>
-        &times;
-      </button>
-
-      {isOpen && (
-        <div className="steps">
-          <div className="numbers">
-            <div className={`${step >= 1 ? "active" : ""}`}>1</div>
-            <div className={`${step >= 2 ? "active" : ""}`}>2</div>
-            <div className={`${step >= 3 ? "active" : ""}`}>3</div>
-          </div>
-
-          <p className="message">
-            step {step} : {messages[step - 1]}
-          </p>
-
-          <div className="buttons">
-            <button
-              style={{ backgroundColor: "#7950f2", color: "#fff" }}
-              onClick={handlePrevious}
-            >
-              Previous
-            </button>
-            <button
-              style={{ backgroundColor: "#7950f2", color: "#fff" }}
-              onClick={handleNext}
-            >
-              Next
-            </button>
-          </div>
-        </div>
-      )}
-    </>
+    <div className="container">
+      <Count />
+    </div>
   );
 }
 
+function Count() {
+  const [count, setCount] = useState(0);
+
+  const date = new Date("june 21 2027");
+  date.setDate(date.getDate() + count);
+
+  function handleNext() {
+    setCount(count + 1);
+  }
+
+  function handlePrevious() {
+    setCount(count - 1);
+    console.log(count);
+  }
+  return (
+    <div>
+      <button onClick={handlePrevious}>-</button>
+      <span>count:{count}</span>
+      <button onClick={handleNext}>+</button>
+      <p className="message">
+        <span>
+          {count === 0
+            ? `Today is `
+            : count > 0
+            ? `${count} days from today is `
+            : `${Math.abs(count)} days ago was`}
+        </span>
+        {date.toDateString()}
+      </p>
+    </div>
+  );
+}
 export default App;
